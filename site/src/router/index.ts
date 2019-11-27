1172ad4e7058f62b227c68bf7e197c4f6d08462a
Nicolas Hassiakis <40218955+nhassiakis@users.noreply.github.com>
2019-11-27 18:02:18 +0100

    Changed routes

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/site/Home.vue'
+import About from '../views/site/About.vue'
+import Contact from '../views/site/Contact.vue'
+
 
 Vue.use(VueRouter)
 
@@ -13,10 +16,16 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    component: About,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/site/About.vue')
+  },
+  {
+    path: '/',
+    name: 'contact',
+    component: Contact
   }
 ]
 
---
+++ site/src/views/site/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>This is a contact page</p>
+    </div>
+</template>
